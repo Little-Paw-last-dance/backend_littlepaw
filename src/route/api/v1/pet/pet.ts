2a8d30807fea9cdc3662997f4096b474b5d1867a
Dylan-Chambi <dylanchambifi@gmail.com>
2024-05-01 07:06:26 -0400

    Get all pets endpoint with auth middleware

--- src/route/api/v1/pet/pet.ts
+++ src/route/api/v1/pet/pet.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { authenticationValidation } from "../../../../middleware/authValidation";
-import { postPet } from "../../../../controller/petController";
+import { getPets, postPet } from "../../../../controller/petController";
 import { petPostValidationMiddleware } from "../../../../middleware/pet";
 
 const petRouter = express.Router();
 
 petRouter.post("/", [petPostValidationMiddleware, authenticationValidation], postPet);
+petRouter.get("/", [authenticationValidation], getPets);
 
 export default petRouter;
