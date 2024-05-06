759f2e1d8e40f1f284e16842df917d6c1025f33b
ElJoamy <josephsalguero1@upb.edu>
2024-05-06 07:36:28 -0400

    chore: Update authentication middleware to use logger for token validation errors

--- src/middleware/authValidation.ts
+++ src/middleware/authValidation.ts
@@ -5,11 +5,13 @@ import HttpException from '../exception/HttpException';
 import typeORM from '../db/dataSource';
 import User from '../entity/User';
 import Role from '../entity/Roles';
+import logger from '../config/logger';
 
 export const authenticationValidation = (req: Request, res: Response, next: NextFunction) => {
   const accessToken = req.headers.authorization?.split(' ')[1];
 
   if (!accessToken) {
+    logger.warn('Invalid or missing token');
     res.status(400).json(new HttpException('Invalid or missing token', 400));
   } else {
     auth
@@ -20,7 +22,7 @@ export const authenticationValidation = (req: Request, res: Response, next: Next
         next();
       })
       .catch(error => {
-        console.log(error);
+        logger.error('Error verifying token: %o', error);
         if (error.code === 'auth/id-token-expired') {
           res.status(401).json(new HttpException('Token expired', 401));
         } else {
@@ -34,6 +36,13 @@ export const authenticationValidation = (req: Request, res: Response, next: Next
 export const getRoles = async (req: Request, res: Response, next: NextFunction) => {
   const firebaseUser: FirebaseUser = res.locals.firebaseUser;
   const userRoles = await typeORM.manager.findOne<User>('User', { where: { email: firebaseUser.email }, relations: ['roles'] });
+
+  if (!userRoles) {
+    logger.warn('No roles found for user: %s', firebaseUser.email);
+    res.status(404).json(new HttpException('User roles not found', 404));
+    return;
+  }
+
   res.locals.roles = userRoles?.roles;
   next();
 };
